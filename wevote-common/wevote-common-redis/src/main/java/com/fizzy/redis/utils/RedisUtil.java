a23f7cbb67410f5e46d10784547d88f80affea7d
renjx <jinxian.ren@hand-china.com>
2022-03-29 09:57:30 +0800

    修改pom打包插件

--- wevote-common/wevote-common-redis/src/main/java/com/fizzy/redis/utils/RedisUtil.java
+++ wevote-common/wevote-common-redis/src/main/java/com/fizzy/redis/utils/RedisUtil.java
@@ -1,6 +1,5 @@
 package com.fizzy.redis.utils;
 
-import com.fizzy.core.entity.PostHeat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
