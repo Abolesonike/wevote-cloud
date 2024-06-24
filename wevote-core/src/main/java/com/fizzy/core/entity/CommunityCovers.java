8d167c414f10f8e34f304b3d127e036fc1505f57
renjinxian <jinxian.ren@hand-china.com>
2024-06-24 18:01:23 +0800

    添加缺失的类

---
+++ wevote-core/src/main/java/com/fizzy/core/entity/CommunityCovers.java
@@ -0,0 +1,10 @@
+package com.fizzy.core.entity;
+
+import lombok.Data;
+
+@Data
+public class CommunityCovers {
+    private Integer id;
+    private Integer communityId;
+    private String coverUrl;
+}
